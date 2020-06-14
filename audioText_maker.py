ebe220af331a3adf2f519e33f4096e837c1a8ae3
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-06-14 20:11:51 +0200

    Update audioText_maker.py

--- audioText_maker.py
+++ audioText_maker.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 import tkinter.scrolledtext as sct
 from langs_dict import langs
 import gtts
+import threading
 import os
 
 class app():
@@ -16,12 +17,12 @@ class app():
         self.entry = sct.ScrolledText(self.window,width=69,height=8)
         self.entry.place(x=10,y=20)
         self.btnCreate = Button(self.window,text='CREATE AUDIO-TEXT',width=81)
-        self.btnCreate.place(x=10,y=200)
+        self.btnCreate.place(x=10,y=175)
         self.btnClear = Button(self.window,text='CLEAR TEXT',width=81)
-        self.btnClear.place(x=10,y=236)
-        self.frame = Frame(self.window,bg='black')
+        self.btnClear.place(x=10,y=205)
+        self.btnTranslate = Button(self.window,text='TRANSLATE TEXT',width=81)
+        self.btnTranslate.place(x=10,y=235)
         self.lang = 'en'
-        
         self.scrollbar = Scrollbar(orient=VERTICAL)
         self.scrollbar.pack(side=RIGHT,fill=Y)
         self.entryLang = Listbox(self.window,width=26,height=15)
@@ -37,9 +38,17 @@ class app():
 
         self.window.mainloop()
 
+    def make_audio(self):
+        self.lang = entryLang.get()
+        self.tts = gtts.gTTS(self.entry.get(),lang=self.lang)
+        self.tts.save("TextAudio.mp3")
+
     def insertb(self):
         for i in self.valores:
             self.entryLang.insert(END,i)
+
+    def init_audio(self):
+        t = threading.Thread(target=self.make_audio)
 
 if __name__=="__main__":
     app()
