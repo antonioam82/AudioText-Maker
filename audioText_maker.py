3ecc562e5636c13bb675b155892e1b9f077c4fc9
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-06-14 14:40:48 +0200

    Update audioText_maker.py

--- audioText_maker.py
+++ audioText_maker.py
@@ -10,10 +10,14 @@ class app():
         self.window = Tk()
         self.window.title("AudioText Maker")
         self.window['bg'] = 'gainsboro'
-        self.window.geometry("593x500")
+        self.window.geometry("773x430")
 
         self.entry = sct.ScrolledText(self.window,width=69,height=8)
         self.entry.place(x=10,y=20)
+        self.btnTrans = Button(self.window,text='TRANSLATE')
+        self.btnTrans.place(x=10,y=225)
+        self.entryLang = ttk.Combobox(self.window,width=24,state='readonly')
+        self.entryLang.place(x=594,y=20)
 
 
         self.window.mainloop()
