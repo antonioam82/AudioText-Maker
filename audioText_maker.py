a8635d85428e2b137ab97f6d0b76ff6d1a7cfd16
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-06-14 21:19:57 +0200

    Update audioText_maker.py

--- audioText_maker.py
+++ audioText_maker.py
@@ -5,6 +5,7 @@ import tkinter.scrolledtext as sct
 from langs_dict import langs
 import gtts
 import threading
+from googletrans import Translator
 import os
 
 class app():
@@ -13,6 +14,7 @@ class app():
         self.window.title("AudioText Maker")
         self.window['bg'] = 'gainsboro'
         self.window.geometry("774x276")
+        self.translator = Translator()
 
         self.entry = sct.ScrolledText(self.window,width=69,height=8)
         self.entry.place(x=10,y=20)
@@ -20,14 +22,15 @@ class app():
         self.btnCreate.place(x=10,y=175)
         self.btnClear = Button(self.window,text='CLEAR TEXT',width=81)
         self.btnClear.place(x=10,y=205)
-        self.btnTranslate = Button(self.window,text='TRANSLATE TEXT',width=81)
+        self.btnTranslate = Button(self.window,text='TRANSLATE TEXT',width=81,command=self.init_translation)
         self.btnTranslate.place(x=10,y=235)
         self.lang = 'en'
+        self.text = ""
+        
         self.scrollbar = Scrollbar(orient=VERTICAL)
         self.scrollbar.pack(side=RIGHT,fill=Y)
         self.entryLang = Listbox(self.window,width=26,height=15)
         self.entryLang.place(x=594,y=20)
-        
         self.entryLang.config(yscrollcommand = self.scrollbar.set)
         self.scrollbar.config(command = self.entryLang.yview)
         
@@ -37,6 +40,17 @@ class app():
         self.insertb()
 
         self.window.mainloop()
+        
+    def translate(self):
+        #
+        self.text = self.entry.get('1.0',END)
+        self.entry.delete('1.0',END)
+        self.position = (self.entryLang.curselection())[0]
+        self.lang = self.claves[(self.valores).index(self.entryLang.get(int(self.position)))] #self.entryLang.get(2)
+        print(self.lang)
+        self.translation = (self.translator.translate(self.text,dest=self.lang).text)
+        print(self.translation)
+        self.entry.insert(END,self.translation)
 
     def make_audio(self):
         self.lang = entryLang.get()
@@ -47,8 +61,13 @@ class app():
         for i in self.valores:
             self.entryLang.insert(END,i)
 
+    def init_translation(self):
+        t1 = threading.Thread(target=self.translate)
+        t1.start()
+
     def init_audio(self):
         t = threading.Thread(target=self.make_audio)
+        t.start()
 
 if __name__=="__main__":
     app()
